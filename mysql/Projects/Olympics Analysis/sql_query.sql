b09db1a241e2d3a95b3f7afcb358b1e3a9e7efac
arynch24 <arynch24@gmail.com>
2024-03-31 16:37:26 +0530

    olympics analysis

--- mysql/Projects/Olympics Analysis/sql_query.sql
+++ mysql/Projects/Olympics Analysis/sql_query.sql
@@ -1,37 +1,63 @@
 create database olympics_analysis;
 use olympics_analysis;
 
+#A
 select * from olympics;
 
+#B
 select name as Athelete_Name, medal as Medals 
 from olympics;
 
-select count(name) as Athelete_Count
+#C
+select count(distinct name) as Athelete_Count
 from olympics;
 
+#D
 select * from olympics
 where medal="Gold";
 
+#F
 select name as Athelete_Name ,year
 from olympics
 where medal="Silver"
 order by year;
+
+#F
 
 select distinct team as Country , count(medal) as Medal_Count
 from olympics
 where medal in ("Gold", "Silver","Bronze")
 group by  team;
 
-select distinct team as Country , count(medal) as Gold_Medal
+#G
+select team as Country 
 from olympics
 where medal ="Gold"
-group by  team;
+group by  team
+having count(medal)>50;
 
-
-select distinct name as Athelete_Name, count(medal) as Medal_count
+#H
+select , count(*) as Medal_count
 from olympics
 group by name
 order by count(medal) desc;
 
+#I
+select name as Athelete_Name , count(*) as Total_Medals
+from olympics
+group by name
+order by Total_Medals desc
+limit 1;
 
+#J
+select distinct event 
+from olympics
+where event like "%Freestyle%";
+
+#K
+select sport , name as Athelete_Name, count(*) as Total_Medals
+from olympics 
+group by sport ,name
+order by Total_Medals desc
+limit 3;
  
